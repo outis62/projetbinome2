7ab5e636731d0be3d453876566eec339cfc3959c
ZABREBOUREIMA <zabreboureima236@gmail.com>
2023-04-24 14:47:02 +0200

    first commit

---
+++ script/index.js
@@ -0,0 +1,20 @@
+(function($){
+    // variables
+    elementWidth = $('ul').width(),
+    containerWidth = $('nav').width(),
+    difference = elementWidth-containerWidth,
+    finalWidth = difference * 1.5,
+    element = $('ul');
+    
+    // active on click
+    $('li').on('click', function(){
+      $('li').removeClass('active');
+      $(this).addClass('active');
+    });
+    
+  })(jQuery);
+  
+  // pour le slide de l'accueil
+  $('#myCarousel').carousel({
+    interval: 2000,
+})
